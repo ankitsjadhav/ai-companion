03c5d6fa6d5e36b81b4e82d0b48fd49d9c210c97
ankitsjadhav <ankitsj7@gmail.com>
2025-10-04 16:47:37 +0530

    feat: add companion API and POST request

--- app/(root)/(routes)/companion/[companionId]/components/companionForm.jsx
+++ app/(root)/(routes)/companion/[companionId]/components/companionForm.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios, { Axios } from "axios";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
@@ -25,6 +26,8 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.`;
 
@@ -62,6 +65,8 @@ const formSchema = z.object({
 });
 
 export const CompanionForm = ({ initialData, categories }) => {
+  const router = useRouter();
+
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
@@ -76,7 +81,20 @@ export const CompanionForm = ({ initialData, categories }) => {
 
   const isLoading = form.formState.isSubmitting;
   const onSubmit = async (values) => {
-    console.log(values);
+    try {
+      if (initialData) {
+        await axios.patch(`/api/companion/${initialData.id}`, values);
+      } else {
+        await axios.post("/api/companion/", values);
+      }
+
+      toast.success("Success");
+
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
   };
 
   return (
--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
