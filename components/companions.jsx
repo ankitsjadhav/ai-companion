685d3767cfef94dc847cb0e61928a50f7bf088e8
ankitsjadhav <ankitsj7@gmail.com>
2025-10-06 12:32:39 +0530

    fix

--- components/companions.jsx
+++ components/companions.jsx
@@ -20,7 +20,7 @@ export const Companions = ({ data }) => {
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10">
       {data.map((item) => (
         <Card
-          key={item.name}
+          key={item.id}
           className="bg-primary/10 rounded-xl cursor-pointer hover:opacity-75 transition border-0"
         >
           <Link href={`/chat/${item.id}`}>
