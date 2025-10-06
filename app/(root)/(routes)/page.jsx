124b38cf0e8abc6139069b34fdecd1cdc0812a4f
ankitsjadhav <ankitsj7@gmail.com>
2025-10-06 12:20:29 +0530

    feat: companion list

--- app/(root)/(routes)/page.jsx
+++ app/(root)/(routes)/page.jsx
@@ -1,13 +1,34 @@
+import { Companions } from "@/components/companions";
 import { Categories } from "@/components/categories";
 import { SearchInput } from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
 
-const RootPage = async () => {
+const RootPage = async ({ searchParams }) => {
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+
   const categories = await prismadb.category.findMany();
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
   );
 };
